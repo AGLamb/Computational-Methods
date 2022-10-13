8d7d20102707610a230f0f23d4153c7530cdd3fa
iGGnite <36937528+iGGnite@users.noreply.github.com>
2022-10-13 17:30:31 +0200

    Got further with Ex3b but have not been able to put eps_star columns together in a DataFrame

--- Assignment 2/Bootstrap.py
+++ Assignment 2/Bootstrap.py
@@ -2,12 +2,13 @@ from scipy.stats import norm
 import statsmodels.api as sm
 import pandas as pd
 import numpy as np
-import random
+import random as rnd
 
 
 def main():
     df_X, df_Y = Process_data()  # get X and Y
-    naiveTtest(df_X, df_Y)
+    # naiveTtest(df_X, df_Y)
+    print(np_res_bstrap(df_Y, df_X))
     return
 
 
@@ -23,7 +24,7 @@ def naiveTtest(df_X, df_Y):  # Ex 2 t-test
         Model, y_res = Regress_OLS(df_Y[i], df_X)  # OLS on a column vector of y (25,1) and all of X (25,3)
         Tn = Model.params[2] / np.sqrt(Model.cov_HC0[2][2])  # t-stat w/ b_OLS,2 and s_2^2
 
-        if abs(Tn) >= abs(norm.ppf(alpha / 2)):  # if rejected
+        if abs(Tn) >= abs(norm.ppf(alpha / 2)):  # if Tn > inverse normal dist with alpha/2
             rejected += 1
     print(f' Test rejects H0 approximately: {(rejected / n) * 100:.2f}%')
 
@@ -35,8 +36,14 @@ def Regress_OLS(Dependent, Independent):
     return Results, Results.resid
 
 
-def np_res_bstrap(df_y, df_x):  # first find beta, then residuals. Not sure how to satisfy H0
-    eps_hat = df_y - Regress_OLS()[0].params*df_x
+def np_res_bstrap(df_y, df_x):  # Ex 3b
+    eps_hat = df_y - df_x.dot(np.array(Regress_OLS(df_y, df_x)[0].params))  # eps_hat from beta_OLS, column vectors
+    n = len(eps_hat.columns)
+    df = pd.DataFrame()
+    for i in range(n):
+        eps_star = eps_hat[rnd.randint(0, n)]
+        df = pd.concat([df, eps_star], axis=1)  # Haven't figured out how to put eps_star side by side
+    return df
 
 
 if __name__ == "__main__":
