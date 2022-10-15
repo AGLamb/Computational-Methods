9274139061a845ecda6ea711bdea07757ca60306
Andres <andres@andresguzco.com>
2022-10-16 01:25:35 +0200

    Update

--- Assignment 2/Bootstrap.py
+++ Assignment 2/Bootstrap.py
@@ -12,6 +12,18 @@ alpha = 0.05
 
 
 def main():
+    # Exercise_1()
+    # Exercise_2()
+    return
+
+
+def Exercise_2A():
+    df_X, df_Y = Process_data_2()
+    df_X, df_Y = df_X.to_numpy(), df_Y.to_numpy()
+    return
+
+
+def Exercise_1():
     df_X, df_Y = Process_data()
     df_X, df_Y = df_X.to_numpy(), df_Y.to_numpy()
 
@@ -20,12 +32,12 @@ def main():
     print(f' Test rejects H0 is approximately: {naive_rate * 100:.2f}%')
     print(f' The p-value is approximately: {naive_pvalue:.2f}')
 
-    NP_rate = type_bootstrap(df_Y, df_X, "np", test_stat)
+    NP_rate = type_bootstrap(df_Y, df_X, "np")
     NP_pvalue = 1 - NP_rate
     print(f' Test rejects H0 is approximately: {NP_rate:.2f}%')
     print(f' The p-value is approximately: {NP_pvalue:.2f}')
 
-    Wild_rate = type_bootstrap(df_Y, df_X, "wild", test_stat)
+    Wild_rate = type_bootstrap(df_Y, df_X, "wild")
     Wild_pvalue = 1 - Wild_rate
     print(f' Test rejects H0 is approximately: {Wild_rate * 100:.2f}%')
     print(f' The p-value is approximately: {Wild_pvalue:.2f}')
@@ -37,7 +49,11 @@ def main():
     return
 
 
-def Process_data():
+def Process_data_2():
+    return pd.read_csv('Timeseries_het.txt', header=None), pd.read_csv('Timeseries_dep.txt', header=None)
+
+
+def Process_data_1():
     return pd.read_csv('Regressors.txt', header=None), pd.read_csv('Observables.txt', header=None)
 
 
@@ -47,7 +63,6 @@ def naiveTtest(df_X, df_Y):
 
     for i in range(n):
         Tn = test_stat(df_Y[:, i], df_X)
-
         if abs(Tn) >= abs(t.ppf(alpha / 2, len(df_Y) - 2)):
             rejected += 1
 
@@ -95,7 +110,7 @@ def pair_bootstrap(df_y, df_x):
     return Rej_Rate
 
 
-def type_bootstrap(df_y, df_x, bootstrap_type, function):
+def type_bootstrap(df_y, df_x, bootstrap_type):
     rejected = 0
     n = df_y.shape[1]
 
@@ -104,7 +119,7 @@ def type_bootstrap(df_y, df_x, bootstrap_type, function):
         y_hat = Model.fittedvalues
         vType = 1
 
-        Tn = function(df_y[:, i], df_x)
+        Tn = test_stat(df_y[:, i], df_x)
         if abs(Tn) >= abs(t.ppf(alpha / 2, len(df_y) - 2)):
             rejected += 1
 
@@ -121,7 +136,7 @@ def type_bootstrap(df_y, df_x, bootstrap_type, function):
                 y_star.append(y_star_i)
 
             y_star = np.array(y_star)
-            Tn = function(y_star, df_x)
+            Tn = test_stat(y_star, df_x)
 
             if abs(Tn) >= abs(t.ppf(alpha / 2, len(df_y) - 1)):
                 rejected += 1
